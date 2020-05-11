b4058c352bd454c1b471cfafa7260a21549379fe
Florent Bécart <florent.becart@gmail.com>
2020-05-11 20:07:53 +0300

    Update Zinoma to version 0.3.0 (adds Linux support)

--- HomebrewFormula/zinoma.rb
+++ HomebrewFormula/zinoma.rb
@@ -1,11 +1,16 @@
 require  'formula'
 class Zinoma < Formula
-  version '0.2.0'
+  version '0.3.0'
   desc "Make your build flow incremental"
   homepage "https://github.com/fbecart/zinoma"
 
-  url "https://github.com/fbecart/zinoma/releases/download/#{version}/zinoma-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "5c83c1d4837ecab5b21da9d3491c4379151b282da68c96c9095107cf7b99f8e3"
+  if OS.mac?
+    url "https://github.com/fbecart/zinoma/releases/download/#{version}/zinoma-#{version}-x86_64-apple-darwin.tar.gz"
+    sha256 "99b9094a1fcd28ed82b2bece1f368dcd466b696eabe8e05e3a02d32cd2eb442a"
+  elsif OS.linux?
+    url "https://github.com/fbecart/zinoma/releases/download/#{version}/zinoma-#{version}-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "23ecf84c0f10dd5247666e65594b408ffad8efbe062b36c9e237cc63bff8559e"
+  end
 
   def install
     bin.install "zinoma"
