3e1542ec80b98b5baa608c7a565e3a04bbf14c12
fbecart <fbecart@users.noreply.github.com>
2020-06-11 13:32:53 +0000

    Update Žinoma to version 0.15.3

--- HomebrewFormula/zinoma.rb
+++ HomebrewFormula/zinoma.rb
@@ -1,14 +1,14 @@
 class Zinoma < Formula
-  version '0.15.2'
+  version '0.15.3'
   desc "Make your build flow incremental"
   homepage "https://github.com/fbecart/zinoma"
 
   if OS.mac?
-    url "https://github.com/fbecart/zinoma/releases/download/0.15.2/zinoma-0.15.2-x86_64-apple-darwin.tar.gz"
-    sha256 "73a4c347db6ee6c5f2491bf766ea1e3f8f65ea9f0865632716905d567283bb07"
+    url "https://github.com/fbecart/zinoma/releases/download/0.15.3/zinoma-0.15.3-x86_64-apple-darwin.tar.gz"
+    sha256 "6f5a66d8d8a5a0751655a28d869dcdf6814e927a3b984bc650417f32bea4cac1"
   elsif OS.linux?
-    url "https://github.com/fbecart/zinoma/releases/download/0.15.2/zinoma-0.15.2-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "f153fc8bd5501838a88f6af505fcba21457accf770d0f5b9740f85b5bbc7ddbd"
+    url "https://github.com/fbecart/zinoma/releases/download/0.15.3/zinoma-0.15.3-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "3150513a342e4b6a3d0772c8c36c616b47993919685733a7dca303cbe75d5a34"
   end
 
   def install
